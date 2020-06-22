1f3075c2044d1d1ce0510f126b1899c03e56b7a2
Marta <marta.anthony1@gmail.com>
2020-06-22 15:11:06 -0700

    setting up apis

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ function searchRoute(request, response){
     limit : 1
   }
 
-  superagent(url)
+  superagent.get(url)
     .query(searchParams)
     .then(searchData => {
-      console.log(searchData.body.results[0]);
+      // console.log(searchData.body.results[0]);
       let movieId = searchData.body.results[0].id;
       let movieGenres = searchData.body.results[0].genre_ids;
       let movieVotes = searchData.body.results[0].vote_average;
@@ -46,7 +46,55 @@ function searchRoute(request, response){
         page : 1 // possibly add a random element to the page
       }
       superagent.get(idURL)
-    })``
+        .query(idParams)
+        .then(similarData => {
+          let similarMovieArray = [];
+          for(let i=0; i<similarData.body.results.length; i++){
+            similarMovieArray.push(new Movie(similarData.body.results[i]))
+            if(i >= 2) {
+              break;
+            }
+          }
+          // console.log('this is the output', similarMovieArray);
+          let discoverUrl = 'https://api.themoviedb.org/3/discover/movie?';
+          let genreParams = {
+            api_key: movieKey,
+            with_genres: movieGenres
+          }
+          superagent.get(discoverUrl)
+            .query(genreParams)
+            .then(genreData => {
+              let genreMovieArray = [];
+              for (let i=0; i<genreData.body.results.length; i++){
+                genreMovieArray.push(new Movie(genreData.body.results[i]))
+                if(i >= 2) {
+                  break;
+                }
+              }
+              // console.log('this is genre output', genreMovieArray)
+              let votesParams = {
+                api_key: movieKey,
+                vote_average: movieVotes
+              }
+              superagent.get(discoverUrl)
+                .query(votesParams)
+                .then(votesData => {
+                  let votesMovieArray = [];
+                  for (let i=0; i<votesData.body.results.length; i++){
+                    votesMovieArray.push(new Movie(votesData.body.results[i]))
+                    if(i >= 2) {
+                      break;
+                    }
+                  }
+                  // console.log('this is votes output', votesMovieArray)
+                  let finalFrontendArray = [similarMovieArray, genreMovieArray, votesMovieArray];
+                  console.log('this is final frontend array', finalFrontendArray);
+                  response.status(200).send(finalFrontendArray);
+                }).catch(errorCatch);
+            }).catch(errorCatch);
+        }).catch(errorCatch);
+    }).catch(errorCatch);
+
 }
 
 function errorRoute(request, response){
@@ -56,6 +104,24 @@ function errorRoute(request, response){
 
 function errorCatch(err){
   console.error(err);
+}
+
+function Movie(obj) {
+  this.popularity = obj.popularity;
+  this.vote_count = obj.vote_count;
+  this.poster_path = `https://image.tmdb.org/t/p/w500/${obj.poster_path}`;
+  this.video = obj.video;
+  this.id = obj.id;
+  this.backdrop_path = `https://image.tmdb.org/t/p/w500/${obj.backdrop_path}`;
+  this.original_language = obj.original_language;
+  this.original_title = obj.original_title;
+  this.genre_ids = obj.genre_ids;
+  this.title = obj.title;
+  this.vote_average = obj.vote_average;
+  this.overview = obj.overview;
+  if(this.overview.length > 254) this.overview = this.overview.slice(0, 250)+'...';
+  this.release_date = obj.release_date;
+
 }
 
 client.connect()
