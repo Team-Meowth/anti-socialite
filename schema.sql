309d77e0f1f9e3de303e5b0e273cd517f332e941
petsan <psankiewicz@icloud.com>
2020-06-22 15:42:13 -0700

    this time without a bug, ps

--- schema.sql
+++ schema.sql
@@ -4,11 +4,12 @@ DROP TABLE IF EXISTS favorites;
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   created_at TIMESTAMP, 
-  username VARCHAR(255)
+  username VARCHAR(255),
+  favorite_list VARCHAR(255)
 )
 
 CREATE TABLE favorites (
-  favorite_id int references users.favorite_id,
+  favorite_id int references users.favorite_list,
   created_at TIMESTAMP, 
   url_id VARCHAR(255)
 );
