36394a01855436c8dc8f1463b7cc33ce5f9a1b09
MichaelRefvem <mkrefvem@gmail.com>
2020-06-22 12:07:26 -0700

    functional code, basic server is set up

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-'use static'
+'use strict'
+
+const express = require('express');
+const app = express();
+const superagent = require('superagent');
+require('dotenv').config();
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => console.error(err));
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+app.get('/', homeRoute);
+app.all('*', errorRoute);
+
+function homeRoute(request, response){
+  console.log('I am on the console');
+  response.status(200).send('I am on the browser');
+}
+
+function errorRoute(request, response){
+  console.log('Console error message');
+  response.status(404).send('Browser error message, you have been 404d!')
+}
+
+function errorCatch(err){
+  console.error(err);
+}
+
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    })
+  }).catch(errorCatch);
+
