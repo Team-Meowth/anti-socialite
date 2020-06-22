75fc48f55af3e2e77d68566d2783806b817dc551
MichaelRefvem <mkrefvem@gmail.com>
2020-06-22 14:05:00 -0700

    passing the controls to Marta

--- server.js
+++ server.js
@@ -15,11 +15,38 @@ const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
 app.get('/', homeRoute);
+app.get('/search', searchRoute);
 app.all('*', errorRoute);
 
 function homeRoute(request, response){
   console.log('I am on the console');
   response.status(200).send('I am on the browser');
+}
+
+function searchRoute(request, response){
+  let searchString = 'detective pikachu';
+  let url = 'https://api.themoviedb.org/3/search/movie';
+  const movieKey = process.env.MOVIE_API_KEY;
+  const searchParams = {
+    api_key : movieKey,
+    query : searchString,
+    limit : 1
+  }
+
+  superagent(url)
+    .query(searchParams)
+    .then(searchData => {
+      console.log(searchData.body.results[0]);
+      let movieId = searchData.body.results[0].id;
+      let movieGenres = searchData.body.results[0].genre_ids;
+      let movieVotes = searchData.body.results[0].vote_average;
+      let idURL = `https://api.themoviedb.org/3/movie/${movieId}/recommendations`;
+      let idParams = {
+        api_key : movieKey,
+        page : 1 // possibly add a random element to the page
+      }
+      superagent.get(idURL)
+    })``
 }
 
 function errorRoute(request, response){
