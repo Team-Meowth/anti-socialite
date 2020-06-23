5bf5201fabbffe066887f46ddbd870a3be82c48c
petsan <psankiewicz@icloud.com>
2020-06-23 12:12:15 -0700

    schema, seach changes, everybody

--- schema.sql
+++ schema.sql
@@ -1,26 +1,23 @@
 DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXISTS favorites CASCADE;
+DROP TABLE IF EXISTS movies CASCADE;
+DROP TABLE IF EXISTS cache_of_movies CASCADE;
 
 CREATE TABLE users ( 
-  user_id SERIAL PRIMARY KEY, 
+  id SERIAL PRIMARY KEY, 
   created_at TIMESTAMP, 
   username VARCHAR(255)
 );
 
-CREATE TABLE favorites (
-  user_id int references users(user_id),
-  url_to_favorite_item int
-);
-
 CREATE TABLE movies (
-  movie_id SERIAL PRIMARY KEY,
-  -- url_to_favorite_item int references favorites(url_to_favorite_item),
+  id SERIAL PRIMARY KEY,
   created_at TIMESTAMP, 
   url_to_favorite_item VARCHAR(255),
   popularity real,
   vote_count int,
   video boolean,
   poster_path VARCHAR(255),
-  id int,
+  movie_id INT UNIQUE,
   adult boolean,
   backdrop_path VARCHAR(255),
   original_language VARCHAR(255),
@@ -29,5 +26,29 @@ CREATE TABLE movies (
   title VARCHAR(255),
   vote_average REAL,
   overview VARCHAR(8000),
-  release_date DATE
+  release_date DATE,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+-- CREATE TABLE favorites (
+--   id SERIAL PRIMARY KEY,
+--   url_to_favorite_item int,
+--   user_id INT NOT NULL,
+--   FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
+
+CREATE TABLE cache_of_movies (
+  id SERIAL PRIMARY KEY,
+  movie_url VARCHAR(255),
+  movie_id INT,
+  FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+  url_to_favorite_item int
+);
+
+-- SELECT * FROM favorites JOIN users ON favorites.user_id = users.id;
+
+SELECT * FROM movies JOIN users ON movies.user_id = users.id;
+
+SELECT * FROM movies JOIN cache_of_movies ON movies.movie_id = cache_of_movies.movie_id;
+
