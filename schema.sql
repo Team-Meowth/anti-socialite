3f9cd3e176d3b48463febcba144784fd502102e6
petsan <psankiewicz@icloud.com>
2020-06-23 15:55:10 -0700

    implement sql schema and homepage random, ps mr

--- schema.sql
+++ schema.sql
@@ -1,23 +1,19 @@
 DROP TABLE IF EXISTS users CASCADE;
--- DROP TABLE IF EXISTS favorites CASCADE;
 DROP TABLE IF EXISTS movies CASCADE;
-DROP TABLE IF EXISTS cache_of_movies CASCADE;
+
 
 CREATE TABLE users ( 
-  id SERIAL PRIMARY KEY, 
-  created_at TIMESTAMP, 
+  id SERIAL PRIMARY KEY,  
   username VARCHAR(255)
 );
 
 CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
-  created_at TIMESTAMP, 
-  url_to_favorite_item VARCHAR(255),
   popularity real,
   vote_count int,
   video boolean,
   poster_path VARCHAR(255),
-  movie_id INT UNIQUE,
+  movie_id INT,
   adult boolean,
   backdrop_path VARCHAR(255),
   original_language VARCHAR(255),
@@ -31,24 +27,4 @@ CREATE TABLE movies (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- CREATE TABLE favorites (
---   id SERIAL PRIMARY KEY,
---   url_to_favorite_item int,
---   user_id INT NOT NULL,
---   FOREIGN KEY (user_id) REFERENCES users(id)
--- );
-
-CREATE TABLE cache_of_movies (
-  id SERIAL PRIMARY KEY,
-  movie_url VARCHAR(255),
-  movie_id INT,
-  FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
-  url_to_favorite_item int
-);
-
--- SELECT * FROM favorites JOIN users ON favorites.user_id = users.id;
-
 SELECT * FROM movies JOIN users ON movies.user_id = users.id;
-
-SELECT * FROM movies JOIN cache_of_movies ON movies.movie_id = cache_of_movies.movie_id;
-
