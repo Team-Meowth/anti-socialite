e5d6c7cf829ccbe03e9f3b38c3d32988c4b41ab0
MichaelRefvem <mkrefvem@gmail.com>
2020-06-25 17:49:23 -0700

    button changes on favorites and watchlist

--- seed.sql
+++ seed.sql
@@ -3,29 +3,3 @@ insert into users (username) values ('michael');
 insert into users (username) values ('leah');
 insert into users (username) values ('peter');
 insert into users (username) values ('trevor');
-
-  
-INSERT INTO movies ( 
-  popularity, 
-  poster_path, 
-  backdrop_path, 
-  movie_id, 
-  title, 
-  vote_average, 
-  overview, 
-  release_date, 
-  user_rating, 
-  user_id 
-  ) VALUES (
-  23.742,
-  'https://image.tmdb.org/t/p/w500/wgQ7APnFpf1TuviKHXeEe3KnsTV.jpg',
-  'https://image.tmdb.org/t/p/w500/nDP33LmQwNsnPv29GQazz59HjJI.jpg',
-  447404,
-  'Pokémon Detective Pikachu',
-  7,
-  'In a world where people collect pocket-size monsters (Pokémon) to do battle, a boy comes across an intelligent monster who seeks to be a detective.',
-  -- '2019-05-03',
-  2019,
-  10,
-  1
-  );
